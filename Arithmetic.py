f520122d638da6885341ec3624c15a049620b928
abhay pratap singh <2024d2r043@mietjammu.in>
2025-11-12 10:24:18 +0530

    Create Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,10 @@
+num1 = int (input ("Enter first number!"))
+num2 = int (input ("Enter second number!"))
+sum = num1 + num2
+diff = num1 - num2
+product = num1 * num2
+remainder = num1 % num2
+Print ("Addition:", Sum)
+Print ("Subtraction:", diff)
+Print ("product:", product)
+Print ("remainder:", remainder)
